5c79963567bccda2302f0c11a22e83bb776f93b5
Aladinebn <93163506+Aladinebn@users.noreply.github.com>
2022-07-14 18:14:31 +0100

    :racehorse: adding footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Die from "./components/Die";
+import Footer from "rc-footer";
+import "rc-footer/assets/index.css";
 
 export default function App() {
   return (
     <div>
       <Die />
+      <Footer
+        columns={[
+          {
+            title: "🤠 TENZIES APP 🤠",
+          },
+        ]}
+        bottom="Made with ❤️ by ALA EDDINE BENNA"
+      />
+      ,
     </div>
   );
 }
